d359a8d53cdd3378819c4cc2fa2d8aeb8eacbcb3
petrkeil <pkeil@seznam.cz>
2018-11-29 01:05:00 +0100

    Cleaned repository

--- fineraster.r
+++
@@ -1,7 +0,0 @@
-files <- paste("../wc2-5/",
-               list.files("../wc2-5/", pattern = ".bil"),
-               sep = "")
-bio2.5 <- stack(files)
-bio2.5 <- bio2.5[[c(1, 4, 16, 17, 5,6,7,8)]]
-
-
